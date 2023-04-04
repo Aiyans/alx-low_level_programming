64605ca2221b3eeea5d9b85c37c4f0fb4f3283c2
Aiyans <soureiyanoura@iut-dhaka.com>
2023-04-04 13:28:51 -0700

    mems

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * _memset - allow fills memory with a constant byte,
+ * @s:variable of: memory area.
+ * @b: constant byte.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
+ */
 char *_memset(char *s, char b, unsigned int n) {
     char *p = s;
     while (n--) {
